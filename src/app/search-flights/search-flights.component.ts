27358deefc5ad36b519fd65fde9bc535acf07e10
ARGHYA KAMAL SEN <arghyakamal200@gmail.com>
2025-08-17 00:45:27 +0530

    Enhance flight search functionality by adding form controls for from, destination, fromDate, toDate, and numberOfPassengers; update search method to utilize form values.

--- src/app/search-flights/search-flights.component.ts
+++ src/app/search-flights/search-flights.component.ts
@@ -1,17 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FlightService } from '../api/services/flight.service';
 import { Flight } from '../api/models/flight';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-search-flights',
   templateUrl: './search-flights.component.html',
   styleUrl: './search-flights.component.css',
 })
-export class SearchFlightsComponent {
+export class SearchFlightsComponent implements OnInit {
   searchResult: Flight[] = [];
-  constructor(private flightService: FlightService) {}
+  constructor(private flightService: FlightService, private fb: FormBuilder) {}
+  searchForm = this.fb.group({
+    from: [''],
+    destination: [''],
+    fromDate: [''],
+    toDate: [''],
+    numberOfPassengers: [1],
+  });
+
+  ngOnInit(): void {
+    this.search();
+  }
   search() {
-    this.flightService.searchFlight({}).subscribe({
+    this.flightService.searchFlight(this.searchForm.value).subscribe({
       next: (response) => (this.searchResult = response),
       error: this.handleError,
     });
